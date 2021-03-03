ffcf4b08846f481c59b477a0e12b104e6988ebaa
aaroncurtisyoga <aaroncurtisyoga@gmail.com>
2021-03-03 17:29:56 -0500

    Connect to Mongo DB. Setup dev script to run env-cmd

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,37 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const HttpError = require('./models/http-errors')
-const instagramPhotosRoutes = require('./routes/instagram-photos-routes')
-const bookListRoutes = require('./routes/book-list-routes')
+const express = require("express");
+require("./db/mongoose");
+const bodyParser = require("body-parser");
 
-const port = 5000
+const HttpError = require("./models/http-errors");
+const instagramPhotosRoutes = require("./routes/instagram-photos-routes");
+const bookListRoutes = require("./routes/book-list-routes");
+
+const port = process.env.PORT || 5000;
 
 // Create an Express server
-const app = express()
+const app = express();
 
 /*----------*/
 /*Middleware*/
 /*----------*/
-app.use(bodyParser.json())
-app.use('/api/instagram-photos', instagramPhotosRoutes)
-app.use('/api/book-list', bookListRoutes)
+app.use(bodyParser.json());
+app.use("/api/instagram-photos", instagramPhotosRoutes);
+app.use("/api/book-list", bookListRoutes);
 // Handle unsupported routes
 app.use((req, res, next) => {
-    throw new HttpError('Could not find this route.', 404);
-})
+  throw new HttpError("Could not find this route.", 404);
+});
 // Error Handling to Execute if any middleware in front has an error
 app.use((error, req, res, next) => {
-    // Check if a response has already been sent
-    if(res.headerSent) {
-        return next(error)
-    }
-    // No response sent yet, so send one now
-    res.status(error.code || 500)
-    res.json({ message:error.message || 'An unknown error occured' })
-})
+  // Check if a response has already been sent
+  if (res.headerSent) {
+    return next(error);
+  }
+  // No response sent yet, so send one now
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred" });
+});
 
 app.listen(port, () => {
-    console.log(`Server is up on port ${port}`)
-})
+  console.log(`Server is up on port ${port}`);
+});
---
+++ backend/db/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`Connection to Mongoose successful`);
+  })
+  .catch((e) => {
+    console.log(`Connection to Mongoose not successful`);
+    console.log(e);
+  });
