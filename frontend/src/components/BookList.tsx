9ee5504e0a9fb67f600d09b648c27ee9849ecf98
aaroncurtisyoga <aaroncurtisyoga@gmail.com>
2021-03-05 17:24:39 -0500

    Env variable for backend URL

--- frontend/src/components/BookList.tsx
+++ frontend/src/components/BookList.tsx
@@ -4,7 +4,7 @@ import useFetch from "use-http";
 const BookList: React.FC = () => {
   const options = {}; // these options accept all native `fetch` options
   const { loading, error, data } = useFetch(
-    "http://localhost:5000/api/book-list",
+    `${process.env.REACT_APP_BACKEND_URL}/api/book-list`,
     options,
     []
   );
