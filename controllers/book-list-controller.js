bb39bc318cd1ff6d058d512e2b5ed8433bb53233
aaroncurtisyoga <aaroncurtisyoga@gmail.com>
2021-03-18 17:05:44 -0400

    Remove mongo bit for now

--- controllers/book-list-controller.js
+++ controllers/book-list-controller.js
@@ -1,11 +1,11 @@
-const Books = require("../models/google-books-schema");
 const HttpError = require("../models/http-errors");
+const getGoogleBooks = require("../util/google-books");
 
 const getBookList = async (req, res, next) => {
   let googleBooks;
 
   try {
-    googleBooks = await Books.find();
+    googleBooks = await getGoogleBooks();
   } catch (e) {
     console.log(e);
     const error = new HttpError(e, 500);
--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .createConnection(process.env.MONGODB_URL, {
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
