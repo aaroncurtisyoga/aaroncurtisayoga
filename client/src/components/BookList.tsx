36faa71edd62124d81efa81b656c7c23d8c3759a
aaroncurtisyoga <aaroncurtisyoga@gmail.com>
2021-06-15 19:39:45 -0400

    Wire up call to Books API from the Front end

--- client/src/components/BookList.tsx
+++ client/src/components/BookList.tsx
@@ -1,9 +1,12 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import useFetch from "use-http";
-
+export interface IBookList {
+  success: boolean;
+  message: string;
+}
 const BookList: React.FC = () => {
   const options = {}; // these options accept all native `fetch` options
-  const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState<IBookList[] | undefined>(undefined);
   const { get, response } = useFetch(
     `${process.env.REACT_APP_BACKEND_URL}/api/book-list`,
     options
@@ -12,8 +15,12 @@ const BookList: React.FC = () => {
   const loadGoogleBooks = useCallback(async () => {
     const data = await get();
     // Store first 9 images returned from API in state
-    if (response.ok) setBooks(data[0].items.slice(0, 9));
+    if (response.ok) setBooks(data[0].items);
   }, [get, response]);
+
+  useEffect(() => {
+    loadGoogleBooks();
+  }, [loadGoogleBooks]);
 
   return (
     <>
